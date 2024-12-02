558a08a32347ba6319c63f592a257ecc1d2c0ae1
Alexander Ryabtsev <bro-88@mail.ru>
2024-12-02 17:06:01 +0300

    third commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,26 @@
 //  Темплейт карточки
-const cardTemplate            = document.querySelector( '#card-template').content;
+const cardTemplate = document.querySelector('#card-template').content;
 //  DOM узлы
-const pageContentElement      = document.querySelector('.content');
-const placesListElement       = pageContentElement.querySelector('.places__list');
+const pageContentElement = document.querySelector('.content');
+const placesListElement = pageContentElement.querySelector('.places__list');
 //  Функция создания карточки
 
-function createCard(name,link, removeCard ) {
-   const cardNode         = cardTemplate.cloneNode(true);
-   const cardTitle        = cardNode.querySelector('.card__title');
-   const cardImage        = cardNode.querySelector('.card__image');
-   const cardDeleteButton = cardNode.querySelector('.card__delete-button');
-   cardDeleteButton.addEventListener('click',removeCard);
-   cardImage.src          = link;
-   cardTitle.textContent  = name;
-   placesListElement.appendChild(cardNode);
+function createCard(name, link, removeCard) {
+  const cardNode = cardTemplate.cloneNode(true);
+  const cardTitle = cardNode.querySelector('.card__title');
+  const cardImage = cardNode.querySelector('.card__image');
+  const cardDeleteButton = cardNode.querySelector('.card__delete-button');
+  cardDeleteButton.addEventListener('click', removeCard);
+  cardImage.src = link;
+  cardTitle.textContent = name;
+  placesListElement.appendChild(cardNode);
 }
 // Функция удаления карточки
-  const removeCard = (event) => {
-    const parentElement = event.target.parentElement;
-    parentElement.remove();
- }; 
+const removeCard = (event) => {
+  const parentElement = event.target.parentElement;
+  parentElement.remove();
+};
 // Выводим карточки на страницу
-initialCards.forEach((card) => { 
-  createCard(card.name, card.link,removeCard );
-} );
+initialCards.forEach((card) => {
+  createCard(card.name, card.link, removeCard);
+});
