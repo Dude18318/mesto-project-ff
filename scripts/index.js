65d57a082706ea854b7f74a7059077120cf3f480
Alexander Ryabtsev <bro-88@mail.ru>
2024-12-02 17:00:15 +0300

    first commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,26 @@
-// @todo: Темплейт карточки
+//  Темплейт карточки
+const cardTemplate            = document.querySelector( '#card-template').content;
+//  DOM узлы
+const pageContentElement      = document.querySelector('.content');
+const placesListElement       = pageContentElement.querySelector('.places__list');
+//  Функция создания карточки
 
-// @todo: DOM узлы
-
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+function createCard(name,link, removeCard ) {
+   const cardNode         = cardTemplate.cloneNode(true);
+   const cardTitle        = cardNode.querySelector('.card__title');
+   const cardImage        = cardNode.querySelector('.card__image');
+   const cardDeleteButton = cardNode.querySelector('.card__delete-button');
+   cardDeleteButton.addEventListener('click',removeCard);
+   cardImage.src          = link;
+   cardTitle.textContent  = name;
+   placesListElement.appendChild(cardNode);
+}
+// Функция удаления карточки
+  const removeCard = (event) => {
+  const parentElement = event.target.parentElement;
+  parentElement.remove();
+ }; 
+// Выводим карточки на страницу
+initialCards.forEach((card) => { 
+  createCard(card.name, card.link,removeCard );
+} );
